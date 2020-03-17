2829ac62037a5d70db9609e3b812ad03418e262e
Joseph Noonan <noonanjoe92@gmail.com>
2020-03-16 22:39:17 -0700

    Added commenting to file

--- Game/map.py
+++ Game/map.py
@@ -1,13 +1,16 @@
+#Adding necessary include files used map.py
 import items
 import exits
 import rooms
 
 class Map(object): 
     def setup(self):
-        self.all_items = items.populate()
-        self.all_exits = exits.populate()
-        self.all_rooms = rooms.populate()
+        # This will populate the map with the rooms, exits, and items in the different rooms
+        self.all_items = items.populate() # Items from csv to the rooms
+        self.all_exits = exits.populate() # Paths through the map
+        self.all_rooms = rooms.populate() # Different rooms in the game
         
+        # This will iterate through labels and rooms to adds the items and connections
         for label, room in self.all_rooms.iteritems():
-            room.add_items(self.all_items)
-            room.add_exits(self.all_exits)
+            room.add_items(self.all_items) # Adds items where they should be
+            room.add_exits(self.all_exits) # Adds room connections where they should be
